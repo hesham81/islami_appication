01359061a3a6b05f1f12817af8e75b7ca1844602
hesham81 <107004690+hesham81@users.noreply.github.com>
2024-12-31 22:03:17 +0200

    [+] Starting Create Islami Application

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFE2BE7F);
+  static const Color secondaryColor = Color(0xFF202020);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
---
+++ lib/features/splash/page/splash.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  static const routeName = "/splash";
+
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
